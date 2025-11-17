f5114fe5a74363e6bed2643d4683b7eeab7ad124
Alexander Colmorn <Alex802c@gmail.com>
2025-11-17 09:01:23 +0100

    Added keypadController

--- src/Config.h
+++ src/Config.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include <Arduino.h>  
+
+const byte ROWS = 4; // four rows
+const byte COLS = 4; // four columns
+// Map the buttons to an array for the Keymap instance
+char hexaKeys[ROWS][COLS] = {
+
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+
+};
+
+byte colPins[ROWS] = {5, 4, 3, 2}; // Pins used for the rows of the keypad
+byte rowPins[COLS] = {9, 8, 7, 6}; // Pins used for the columns of the keypad
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include "Controllers/DisplayController.h"
+#include "Controllers/KeypadController.h"
 
 DisplayController displayController;
+KeypadController keypadController;
 
 // put function declarations here:
 int myFunction(int, int);
@@ -16,4 +18,5 @@ void loop() {
   delay(2000);
   displayController.writeGameMenu();
   delay(2000);
+
 }
